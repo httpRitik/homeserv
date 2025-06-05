0653b01fa7055374baea9a25b22ba5e9f15f79df
httpRitik <ritikmandal7716@gmail.com>
2025-06-05 09:10:43 +0530

    Fix Expo Router Path Resolution

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['expo-router/babel', { appRoot: './app' }],
+      'expo-router/babel',
       'react-native-reanimated/plugin',
       ['transform-inline-environment-variables'],
     ],
